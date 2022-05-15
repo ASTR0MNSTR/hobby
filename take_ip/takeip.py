0b41afc3b0d8a0c44b5519a062fba65c903d1bfe
ASTR0MNSTR <ryzhovuniv27@gmail.com>
2022-05-15 13:04:25 +0200

    message

--- take_ip/takeip.py
+++ take_ip/takeip.py
@@ -1,11 +1,19 @@
+import string
 import re
 
 
-def take_ip(path_input):
-    with open(path_input, 'r') as f:
-        lines = f.readlines()
-        print(lines)
-
+def take_ip(path_input, path_output):
+    with open(path_input, 'r') as input:
+        lines = input.readlines()
+        sites = [item for ind, item in enumerate(lines) if ind % 3 == 0]
+        sites_out = ''
+        for item in sites:
+            sites_out += re.sub(r'\n', '', item) + ' '
+        print(sites_out)
+        with open(path_output, 'w') as output:
+            output.write(sites_out)
+            output.close()
+    
 
 if __name__ == '__main__':
-    take_ip('C:/hobby/hobby/take_ip/input.txt')
+    take_ip('C:/hobby/hobby/take_ip/input.txt', 'C:/hobby/hobby/take_ip/output.txt')
